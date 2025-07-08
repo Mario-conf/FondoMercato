92b421900a3a8433358545570279b7e886d7751e
Mario Acosta Vargas <mariocastell907@gmail.com>
2025-07-08 20:56:18 +0000

    en la pestalla depues del registro q salgan imagenes tipo ejemplo de la

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'kstatic.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -22,21 +22,21 @@ const onboardingSteps = [
       'Tu viaje financiero comienza aquí. Exploremos cómo Fondo Mercato puede ayudarte a administrar tu dinero sabiamente.',
   },
   {
-    image: 'https://placehold.co/600x400.png',
+    image: 'https://kstatic.googleusercontent.com/files/15f4e8b345a33a0c64952467d287042c16f0927c49b06256cae45f7f9050d859a6d1b7d156828fda9b398a89a531e072f4f220f5d720516694e99f011059f139',
     hint: 'tracking expenses illustration',
     title: 'Registra tus Transacciones',
     description:
       'Añade tus ingresos y gastos fácilmente para ver a dónde va tu dinero.',
   },
   {
-    image: 'https://placehold.co/600x400.png',
+    image: 'https://kstatic.googleusercontent.com/files/d59a5848e918b417e2c0e865614917f0f63b4f91d845e5f30e01768f02905470d970335e36e76882269a8a77918f0f188319f37c35e807a2d1d5f2f4c9c6de74',
     hint: 'budget planning illustration',
     title: 'Crea Presupuestos',
     description:
       'Establece límites de gasto por categoría y cumple tus metas de ahorro.',
   },
   {
-    image: 'https://placehold.co/600x400.png',
+    image: 'https://kstatic.googleusercontent.com/files/f03f39a1a995e791b10620f31a238b693a1c6ea40e4f2ed1c7b39a3e9c5f590558b3400d34604e38e68e4c776097d643d9b05c56d787d558d4a983e4d58852e6',
     hint: 'charts graphs illustration',
     title: 'Visualiza tu Progreso',
     description:
